0f3bd2357068f7d545d4e3378fe6b76239e74dd4
Parshkov <mail@parshkov.com>
2026-09-01 21:22:10 -0700

    R12: export identity service public surface

---
+++ src/identity/__init__.py
@@ -0,0 +1,36 @@
+"""R12 privacy-first identity/auth/consent service."""
+
+from .adapters import BearerAgentAdapter, ManualUIAdapter, WebMCPAdapter
+from .backend import IdentityBackend, R11IdentityBackend
+from .models import (
+    IDENTITY_CONTRACT_VERSION,
+    ActorContext,
+    AuthenticationError,
+    AuthorizationError,
+    ConfirmationRequiredError,
+    ConsentChoices,
+    CsrfError,
+    IdentityError,
+    IdentityValidationError,
+    SessionCredentials,
+)
+from .service import IdentityService
+
+__all__ = [
+    "IDENTITY_CONTRACT_VERSION",
+    "ActorContext",
+    "AuthenticationError",
+    "AuthorizationError",
+    "BearerAgentAdapter",
+    "ConfirmationRequiredError",
+    "ConsentChoices",
+    "CsrfError",
+    "IdentityBackend",
+    "IdentityError",
+    "IdentityService",
+    "IdentityValidationError",
+    "ManualUIAdapter",
+    "R11IdentityBackend",
+    "SessionCredentials",
+    "WebMCPAdapter",
+]
